c5effad3f56c392d357faef11db00ec134ef0cab
d04g145 <tec_suportes@hotmail.com>
2019-03-23 10:05:29 -0300

    Update repository.sh

--- repository.sh
+++ repository.sh
@@ -1,7 +1,6 @@
 // Kali-sources.list-Repositories-update
 //Por motivo de muitos repositorios com link quebrado disponibilisei estes que uso ja um tempo. ok
-//substitui no arquivo ">"
-//vc adiciona ao arquivo ">>"
+
 #!/bin/bash
 echo 'deb http://old.kali.org/kali sana main non-free contrib' >> /etc/apt/sources.list
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
